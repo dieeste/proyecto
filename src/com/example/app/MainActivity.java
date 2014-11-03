36361ef4f45abaffa28966bfa2413882202f9732
diego <diegoesmo_90@hotmail.com>
2014-11-03 18:41:25 +0100

    empezamos

---
+++ src/com/example/app/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+    		
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true; /** true -> el menú ya está visible */
+    }
+    
+    public void lanzarAcercaDe(View view){
+
+	      Intent i = new Intent(this, AcercaDe.class);
+
+	            startActivity(i);
+	 }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+      /*  int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+    	  switch (item.getItemId()) {
+          case R.id.acercaDe:
+                 lanzarAcercaDe(null);
+                 break;
+          }
+          return true; /** true -> consumimos el item, no se propaga*/
+ }
+}
