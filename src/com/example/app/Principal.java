c647b09091fd29eedd5fa54c4942dcb543cbad69
FELIX <felixhuete@iMac-de-FELIX.local>
2014-11-04 18:48:54 +0100

    4/11/14
    Creacion de la pantalla del login (principal)

---
+++ src/com/example/app/Principal.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+
+public class Principal extends Activity implements OnClickListener{
+Button entrar;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		 requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        setContentView(R.layout.principal);
+	        
+	        entrar = (Button) findViewById(R.id.entrar);
+	        
+	        //Agregamos la escucha
+	        entrar.setOnClickListener(this);
+	        
+	}
+
+	@Override
+	public void onClick(View boton) {
+		// TODO Auto-generated method stub
+		Intent cambio=new Intent (".MainActivity");
+			startActivity(cambio);
+		
+		
+	}
+
+}
