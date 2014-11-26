88a66b1f1df599462423b4a0b5698575057e69ae
diego <diegoesmo_90@hotmail.com>
2014-11-26 13:00:29 +0100

    26/11/14
    Añadido comentarios a la app en general.
    Hemos añadido la gráfica y ahora recoge los datos del sensor y los
    representa en tiempo real. En el móvil pequeño funciona bien pero en el
    grande no va del todo bien. Los puntos no se unen pero se ve la forma.
    
    También hemos añadido la biblioteca achartengine-1.1-0.jar para usar las
    funciones del chart

--- src/com/example/app/Grafica.java
+++ src/com/example/app/Grafica.java
@@ -1,135 +1,151 @@
 package com.example.app;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Vector;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.text.format.Time;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
+public class Grafica extends Activity implements SensorEventListener {
 
-public class Grafica extends Activity implements SensorEventListener{
-	
-	 @Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		Parar_sensores();
-	}
+	// Conjunto de datos principal que incluye todas las series de la gráfica
+	XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+	// Render que incluye el renderer personalizado de la gráfica
+	XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
 
+	// Renderer creado más reciente, personalizando las series actuales
+	XYSeriesRenderer mRendererActual;
+	// Gráfica que mostrará los datos
+	GraphicalView mVistaGrafica;
+	// Datos
+	long timestamp;
+	double x;
+	double y;
+	double z;
+	int tipo;
+	// Array
+	ArrayList<Double> datosSensor;
+	// Declaración del layout
+	LinearLayout layout;
+	// Maneja los sensores
+	SensorManager sensorManager;
+	Button parar;
 
-	private XYPlot plot;
-	 SensorManager mSensorManager;
-	 int tipo;
-	 Button parar;
-	 ArrayList <Double> Vector = new ArrayList<Double>();
-	 
-	double DatoX, DatoY;
-	 
-	 
-	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-	
 		setContentView(R.layout.grafica);
-		plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
-		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		Bundle graficas = getIntent().getExtras();
-		tipo = graficas.getInt("tipo");
-		plot.setDomainBoundaries(0, 10, BoundaryMode.FIXED);
-		plot.setRangeBoundaries(-10, 20, BoundaryMode.FIXED);
-		plot.setDomainStepValue(5);
+		// Declaramos objetos
+		layout = (LinearLayout) findViewById(R.id.chart);
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		datosSensor = new ArrayList<Double>();
 		parar = (Button) findViewById(R.id.parar);
-		
-		Iniciar_sensores();
-		
-		
-		
-		
+
+		// Declaramos la escucha del botón parar
 		parar.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				onStop();
+				Parar_sensores();
 			}
 		});
+
+		// Recogemos el tipo de frecuencia que hemos pasado desde la actividad
+		// de acelerómetro
+		Bundle graficas = getIntent().getExtras();
+		tipo = graficas.getInt("tipo");
+
+		// Propiedades de la gráfica
+		mRenderer.setApplyBackgroundColor(true);
+		mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
+		mRenderer.setAxisTitleTextSize(16);
+		mRenderer.setChartTitleTextSize(20);
+		mRenderer.setLabelsTextSize(15);
+		mRenderer.setLegendTextSize(15);
+		mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
+		mRenderer.setZoomButtonsVisible(true);
+		mRenderer.setPointSize(5);
+
+		Iniciar_sensores();
 	}
-	protected void Iniciar_sensores(){
-		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),tipo);
-		
+
+	protected void Iniciar_sensores() {
+		sensorManager
+				.registerListener(this, sensorManager
+						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), tipo);
 	}
-	
-	
-	
-	
-	
-	
-	protected void Parar_sensores(){
-		mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+
+	protected void Parar_sensores() {
+		sensorManager.unregisterListener(this,
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
 	}
-	
-	
-	
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-	
 		synchronized (this) {
-			switch (event.sensor.getType()){
+			switch (event.sensor.getType()) {
+
 			case Sensor.TYPE_ACCELEROMETER:
+				// Series añadidas recientemente
+				XYSeries xSeries = new XYSeries("X");
+				;
+				double x = event.values[0];
+				double y = event.values[1];
+				double z = event.values[2];
+				long timestamp = System.currentTimeMillis();
+
+				xSeries.add(timestamp, x);
+				mDataset.addSeries(xSeries);
+				/*
+				 * mDataset.addSeries(ySeries); mDataset.addSeries(zSeries);
+				 */
+				XYSeriesRenderer xRenderer = new XYSeriesRenderer();
+				xRenderer.setColor(Color.RED);
+				xRenderer.setPointStyle(PointStyle.CIRCLE);
+				xRenderer.setFillPoints(true);
+				xRenderer.setLineWidth(1);
+				xRenderer.setDisplayChartValues(false);
+
+			
+
+				mRenderer.addSeriesRenderer(xRenderer);
+
+				mVistaGrafica = ChartFactory.getLineChartView(this,
+						mDataset, mRenderer);
 				
-				for (int i=0;i<=30; i++){
-					//DatoX = DatoX + 0.5;
-					//Vector.add(DatoX);
-					DatoY = event.values[0];
-					Vector.add(DatoY);
-					
-				}
-			Timer timer =new Timer();
-				timer.scheduleAtFixedRate(new TimerTask() {	
-					@Override
-					public void run() {
-						// TODO Auto-generated method stub
-						XYSeries serie1 = new SimpleXYSeries(Vector, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "X");
-						LineAndPointFormatter serie1Format = new LineAndPointFormatter(Color.rgb(100, 100, 200),Color.rgb(100, 100, 200),null,null);
-						plot.addSeries(serie1, serie1Format);
-					
-						
-						plot.redraw();
-					}
-				}, 0, 150);	
-				//Number[] valoresX = {event.values[0]};
-		
-		break;
+				mVistaGrafica.repaint();
+
+				// Adding the Line Chart to the LinearLayout
+				layout.addView(mVistaGrafica);
 			}
 		}
-		
-	}
-	
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
