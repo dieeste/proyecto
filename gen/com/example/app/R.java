2ff5251e48bab7aa85ae5dbd71059f3c4a8ee5b8
FELIX <felixhuete@iMac-de-FELIX.local>
2014-11-27 20:15:08 +0100

    27/11 FIFO, y vector
    FIFO y vector con tres posiciones, no representa añadida clase para
    representar la grafica y la clase para guardar, todavia no completo.

--- gen/com/example/app/R.java
+++ gen/com/example/app/R.java
@@ -9,9 +9,19 @@ package com.example.app;
 
 public final class R {
     public static final class array {
-        public static final int frecuencia_recogida=0x7f050000;
+        public static final int frecuencia_recogida=0x7f060000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int accele_r=0x7f040003;
+        public static final int accele_x=0x7f040000;
+        public static final int accele_y=0x7f040001;
+        public static final int accele_z=0x7f040002;
+        public static final int background=0x7f040006;
+        public static final int filter_rate_value=0x7f040007;
+        public static final int string=0x7f040005;
+        public static final int zero_line=0x7f040004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,8 +31,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -32,39 +42,39 @@ public final class R {
         public static final int psensor=0x7f020004;
     }
     public static final class id {
-        public static final int TextView01=0x7f090008;
-        public static final int acelerometer=0x7f090012;
-        public static final int acelerometro=0x7f090013;
-        public static final int acercaDe=0x7f09001f;
-        public static final int action_settings=0x7f09001e;
-        public static final int buttons=0x7f09000d;
-        public static final int chart=0x7f09000c;
-        public static final int config=0x7f090020;
-        public static final int detecta=0x7f090019;
-        public static final int ejemplos=0x7f09001d;
-        public static final int enviar=0x7f09000f;
-        public static final int etTiempo=0x7f090007;
-        public static final int giroscopio=0x7f090014;
-        public static final int giroscopo=0x7f090015;
-        public static final int gps=0x7f090004;
-        public static final int grafica=0x7f090000;
-        public static final int guardar=0x7f09000e;
-        public static final int inforacelerometro=0x7f090001;
-        public static final int luminosidad=0x7f09001c;
-        public static final int luminosity=0x7f09001b;
-        public static final int magnetic=0x7f090016;
-        public static final int magnetico=0x7f090017;
-        public static final int medicion=0x7f090009;
-        public static final int parar=0x7f090010;
-        public static final int proximidad=0x7f09001a;
-        public static final int proximity=0x7f090018;
-        public static final int scrollView1=0x7f090011;
-        public static final int sensoresdisponibles=0x7f09000a;
-        public static final int spinnerfrecuencia=0x7f090002;
-        public static final int teoria=0x7f09000b;
-        public static final int tiempos=0x7f090006;
-        public static final int toggleGps=0x7f090003;
-        public static final int toggleTiempo=0x7f090005;
+        public static final int TextView01=0x7f0a0008;
+        public static final int acelerometer=0x7f0a0012;
+        public static final int acelerometro=0x7f0a0013;
+        public static final int acercaDe=0x7f0a001f;
+        public static final int action_settings=0x7f0a001e;
+        public static final int buttons=0x7f0a000d;
+        public static final int chart=0x7f0a000c;
+        public static final int config=0x7f0a0020;
+        public static final int detecta=0x7f0a0019;
+        public static final int ejemplos=0x7f0a001d;
+        public static final int enviar=0x7f0a000f;
+        public static final int etTiempo=0x7f0a0007;
+        public static final int giroscopio=0x7f0a0014;
+        public static final int giroscopo=0x7f0a0015;
+        public static final int gps=0x7f0a0004;
+        public static final int grafica=0x7f0a0000;
+        public static final int guardar=0x7f0a000e;
+        public static final int inforacelerometro=0x7f0a0001;
+        public static final int luminosidad=0x7f0a001c;
+        public static final int luminosity=0x7f0a001b;
+        public static final int magnetic=0x7f0a0016;
+        public static final int magnetico=0x7f0a0017;
+        public static final int medicion=0x7f0a0009;
+        public static final int parar=0x7f0a0010;
+        public static final int proximidad=0x7f0a001a;
+        public static final int proximity=0x7f0a0018;
+        public static final int scrollView1=0x7f0a0011;
+        public static final int sensoresdisponibles=0x7f0a000a;
+        public static final int spinnerfrecuencia=0x7f0a0002;
+        public static final int teoria=0x7f0a000b;
+        public static final int tiempos=0x7f0a0006;
+        public static final int toggleGps=0x7f0a0003;
+        public static final int toggleTiempo=0x7f0a0005;
     }
     public static final class layout {
         public static final int acelerometro=0x7f030000;
@@ -76,35 +86,36 @@ public final class R {
         public static final int teoria=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int menu=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int menu=0x7f090001;
     }
     public static final class string {
-        public static final int acelerometro=0x7f06000e;
-        public static final int acercaDe=0x7f060004;
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int config=0x7f060003;
-        public static final int cuenta=0x7f06000c;
-        public static final int ejemplos=0x7f060006;
-        public static final int empezar=0x7f06000a;
-        public static final int entrar=0x7f060017;
-        public static final int enviar=0x7f060013;
-        public static final int explicacion=0x7f060008;
-        public static final int giroscopio=0x7f060011;
-        public static final int guardar=0x7f060014;
-        public static final int hello_world=0x7f060001;
-        public static final int luminosidad=0x7f06000f;
-        public static final int magnetico=0x7f060012;
-        public static final int medicion=0x7f060005;
-        public static final int mostrar=0x7f06000d;
-        public static final int nombre=0x7f060015;
-        public static final int parar=0x7f06000b;
-        public static final int password=0x7f060016;
-        public static final int proximidad=0x7f060010;
-        public static final int sensores=0x7f060009;
-        public static final int teoria=0x7f060007;
-        public static final int test=0x7f060018;
+        public static final int acelerometro=0x7f07000e;
+        public static final int acercaDe=0x7f070004;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int config=0x7f070003;
+        public static final int cuenta=0x7f07000c;
+        public static final int ejemplos=0x7f070006;
+        public static final int empezar=0x7f07000a;
+        public static final int entrar=0x7f070018;
+        public static final int enviar=0x7f070013;
+        public static final int explicacion=0x7f070008;
+        public static final int giroscopio=0x7f070011;
+        public static final int guardar=0x7f070014;
+        public static final int hello_world=0x7f070001;
+        public static final int luminosidad=0x7f07000f;
+        public static final int magnetico=0x7f070012;
+        public static final int medicion=0x7f070005;
+        public static final int mostrar=0x7f07000d;
+        public static final int nombre=0x7f070016;
+        public static final int parar=0x7f07000b;
+        public static final int password=0x7f070017;
+        public static final int proximidad=0x7f070010;
+        public static final int save_complate=0x7f070015;
+        public static final int sensores=0x7f070009;
+        public static final int teoria=0x7f070007;
+        public static final int test=0x7f070019;
     }
     public static final class style {
         /** 
@@ -128,13 +139,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int MooSpinnerItem=0x7f070003;
-        public static final int MooTextAppearanceSpinnerItem=0x7f070004;
-        public static final int MooTheme=0x7f070002;
+        public static final int AppTheme=0x7f080001;
+        public static final int MooSpinnerItem=0x7f080003;
+        public static final int MooTextAppearanceSpinnerItem=0x7f080004;
+        public static final int MooTheme=0x7f080002;
     }
 }
--- src/com/example/app/Grafica.java
+++ src/com/example/app/Grafica.java
@@ -1,26 +1,33 @@
 package com.example.app;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.chart.PointStyle;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Message;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -34,15 +41,16 @@ public class Grafica extends Activity implements SensorEventListener {
 	// Renderer creado más reciente, personalizando las series actuales
 	XYSeriesRenderer mRendererActual;
 	// Gráfica que mostrará los datos
-	GraphicalView mVistaGrafica;
-	// Datos
-	long timestamp;
-	double x;
-	double y;
-	double z;
+	GraphView mVistaGrafica;
+	
+	private int mBGColor;
+	private int mZeroLineColor;
+	private int mStringColor;
+
 	int tipo;
-	// Array
-	ArrayList<Double> datosSensor;
+	ConcurrentLinkedQueue<float[]> datosSensor = new ConcurrentLinkedQueue<float[]>();
+	float [] xyz= new float[3];
+	
 	// Declaración del layout
 	LinearLayout layout;
 	// Maneja los sensores
@@ -57,9 +65,13 @@ public class Grafica extends Activity implements SensorEventListener {
 		// Declaramos objetos
 		layout = (LinearLayout) findViewById(R.id.chart);
 		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		datosSensor = new ArrayList<Double>();
 		parar = (Button) findViewById(R.id.parar);
 
+		Resources resources = getResources();
+		
+		mStringColor = resources.getColor(R.color.string);
+		mBGColor = resources.getColor(R.color.background);
+		mZeroLineColor = resources.getColor(R.color.zero_line);
 		// Declaramos la escucha del botón parar
 		parar.setOnClickListener(new OnClickListener() {
 
@@ -75,17 +87,9 @@ public class Grafica extends Activity implements SensorEventListener {
 		Bundle graficas = getIntent().getExtras();
 		tipo = graficas.getInt("tipo");
 
-		// Propiedades de la gráfica
-		mRenderer.setApplyBackgroundColor(true);
-		mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-		mRenderer.setAxisTitleTextSize(16);
-		mRenderer.setChartTitleTextSize(20);
-		mRenderer.setLabelsTextSize(15);
-		mRenderer.setLegendTextSize(15);
-		mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
-		mRenderer.setZoomButtonsVisible(true);
-		mRenderer.setPointSize(5);
-
+		//declaramos la grafica
+		mVistaGrafica = new GraphView(this);
+		layout.addView(mVistaGrafica, 0);
 		Iniciar_sensores();
 	}
 
@@ -93,6 +97,7 @@ public class Grafica extends Activity implements SensorEventListener {
 		sensorManager
 				.registerListener(this, sensorManager
 						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), tipo);
+		mVistaGrafica.surfaceCreated(mVistaGrafica.getHolder());
 	}
 
 	protected void Parar_sensores() {
@@ -109,43 +114,219 @@ public class Grafica extends Activity implements SensorEventListener {
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-		synchronized (this) {
+		
 			switch (event.sensor.getType()) {
 
 			case Sensor.TYPE_ACCELEROMETER:
-				// Series añadidas recientemente
-				XYSeries xSeries = new XYSeries("X");
-				;
-				double x = event.values[0];
-				double y = event.values[1];
-				double z = event.values[2];
-				long timestamp = System.currentTimeMillis();
-
-				xSeries.add(timestamp, x);
-				mDataset.addSeries(xSeries);
-				/*
-				 * mDataset.addSeries(ySeries); mDataset.addSeries(zSeries);
-				 */
-				XYSeriesRenderer xRenderer = new XYSeriesRenderer();
-				xRenderer.setColor(Color.RED);
-				xRenderer.setPointStyle(PointStyle.CIRCLE);
-				xRenderer.setFillPoints(true);
-				xRenderer.setLineWidth(1);
-				xRenderer.setDisplayChartValues(false);
-
-			
-
-				mRenderer.addSeriesRenderer(xRenderer);
-
-				mVistaGrafica = ChartFactory.getLineChartView(this,
-						mDataset, mRenderer);
 				
-				mVistaGrafica.repaint();
-
-				// Adding the Line Chart to the LinearLayout
-				layout.addView(mVistaGrafica);
+				
+				for (int i=0;i <3;i++) {
+				float valor = event.values[i];
+				xyz[i] = valor;
+				}
+				
+				synchronized (this) {
+					datosSensor.add(xyz.clone());
+Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
 			}
 		}
 	}
+	private class GraphView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+		
+		private Thread hilo;
+		private SurfaceHolder holder;
+		
+		public GraphView(Context context) {
+			super(context);
+			// TODO Auto-generated constructor stub
+			holder = getHolder();
+			holder.addCallback(this);
 
+			setFocusable(true);
+			requestFocus();
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+
+			
+
+			Paint textPaint = new Paint();
+			textPaint.setColor(mStringColor);
+			textPaint.setAntiAlias(true);
+			textPaint.setTextSize(14);
+
+			Paint zeroLinePaint = new Paint();
+			zeroLinePaint.setColor(mZeroLineColor);
+			zeroLinePaint.setAntiAlias(true);
+
+		
+
+		
+				Canvas canvas = holder.lockCanvas();
+
+			
+
+				canvas.drawColor(mBGColor);
+
+				float zeroLineY = 230;
+
+				synchronized (holder) {
+					float twoLineY = zeroLineY - (20 * 6);
+					float oneLineY = zeroLineY - (10 * 6);
+					float minasOneLineY = zeroLineY + (10 * 6);
+					float minasTwoLineY = zeroLineY + (20 * 6);
+
+					canvas.drawText("2", 5, twoLineY + 5, zeroLinePaint);
+					canvas.drawLine(20, twoLineY, getWidth(), twoLineY,
+							zeroLinePaint);
+
+					canvas.drawText("1", 5, oneLineY + 5, zeroLinePaint);
+					canvas.drawLine(20, oneLineY, getWidth(), oneLineY,
+							zeroLinePaint);
+
+					canvas.drawText("0", 5, zeroLineY + 5, zeroLinePaint);
+					canvas.drawLine(20, zeroLineY, getWidth(), zeroLineY,
+							zeroLinePaint);
+
+					canvas.drawText("-1", 5, minasOneLineY + 5, zeroLinePaint);
+					canvas.drawLine(20, minasOneLineY, getWidth(), minasOneLineY,
+							zeroLinePaint);
+
+					canvas.drawText("-2", 5, minasTwoLineY + 5, zeroLinePaint);
+					canvas.drawLine(20, minasTwoLineY, getWidth(), minasTwoLineY,
+							zeroLinePaint);
+
+					if (datosSensor.size() > 1) {
+						Iterator<float[]> iterator = datosSensor.iterator();
+						float[] before = new float[4];
+						int x = getWidth() - datosSensor.size() * 2;
+						int beforeX = x;
+						x += 2;
+
+						if (iterator.hasNext()) {
+							float[] history = iterator.next();
+							for (int angle = 0; angle < 4; angle++) {
+								before[angle] = zeroLineY
+										- (history[angle] * 6);
+							}
+							while (iterator.hasNext()) {
+								history = iterator.next();
+								for (int angle = 0; angle < 4; angle++) {
+									float startY = zeroLineY
+											- (history[angle] * 6);
+									
+									before[angle] = startY;
+								}
+								beforeX = x;
+								x += 2;
+							}
+						}
+					}
+				}
+
+				holder.unlockCanvasAndPost(canvas);
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					
+				}
+			}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			// TODO Auto-generated method stub
+			hilo = new Thread(this);
+			hilo.start();
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// TODO Auto-generated method stub
+			boolean roop = true;
+			while (roop) {
+					try {
+						hilo.join();
+						roop= false;
+						
+					} catch (InterruptedException e) { }
+			}
+			hilo = null;
+			
+		}
+		
+		
+	}
+	private class SaveThread extends Thread {
+		@Override
+		public void run() {
+			// CSVãƒ‡ãƒ¼ã‚¿ã‚’ä½œæˆ
+			StringBuilder csvData = new StringBuilder();
+			Iterator<float[]> iterator = datosSensor.iterator();
+			while (iterator.hasNext()) {
+				float[] values = iterator.next();
+				for (int angle = 0; angle < 3; angle++) {
+					csvData.append(String.valueOf(values[angle]));
+					if (angle < 3) {
+						csvData.append(",");
+					}
+				}
+				csvData.append("\n");
+			}
+
+			// ãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ãƒ³ã‚°ã®æº–å‚™
+			Message msg = new Message();
+			Bundle bundle = new Bundle();
+
+			try {
+				// SDã‚«ãƒ¼ãƒ‰ã«ãƒ‡ã‚£ãƒ¬ã‚¯ãƒˆãƒªãŒãªã‘ã‚Œã°ä½œæˆ
+				String appName = "app";
+				String dirPath = Environment.getExternalStorageDirectory()
+						.toString()
+						+ "/" + appName;
+				File dir = new File(dirPath);
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+
+				// CSVãƒ•ã‚¡ã‚¤ãƒ«åã‚’ä½œæˆ
+				String fileName = DateFormat.format("yyyyMMddkkmmss", System.currentTimeMillis()).toString().concat(".csv");
+				
+				// CSVãƒ•ã‚¡ã‚¤ãƒ«ã¸ä¿å­˜
+				File file = new File(dirPath, fileName.toString());
+				if (file.createNewFile()) {
+					FileOutputStream fileOutputStream = new FileOutputStream(
+							file);
+					// ãƒ‡ãƒ¼ã‚¿ã‚’æ›¸ãè¾¼ã‚€
+					fileOutputStream.write(csvData.toString().getBytes());
+					fileOutputStream.close();
+				}
+
+				// ãƒ•ã‚¡ã‚¤ãƒ«ã¸ã®å‡ºåŠ›å®Œäº†ã‚’é€šçŸ¥
+				bundle.putString("msg", "GUARDADO");
+				bundle.putBoolean("success", true);
+			} catch (Exception e) {
+			
+
+				// ãƒ•ã‚¡ã‚¤ãƒ«ã¸ã®å‡ºåŠ›å¤±æ•—ã‚’é€šçŸ¥
+				bundle.putString("msg", e.getMessage());
+				bundle.putBoolean("success", false);
+			}
+
+			// ãƒãƒ³ãƒ‰ãƒ©ã‚’ä½¿ã£ã¦ãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ãƒ³ã‚°
+			msg.setData(bundle);
+		
+		}
+	}
 }
+
