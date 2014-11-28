4d6c8f96ddb3ee16fa708d07dcf5b3b0531010f8
diego <diegoesmo_90@hotmail.com>
2014-11-28 13:43:29 +0100

    28/11/14
    YA FUNCIONA TODOOOOOOOOOO!!
    Hemos añadido cosas que ayer no habíamos metido y por eso no nos
    dibujaba.
    También he declarado la escucha de los botones que por eso no hacían
    nada al pulsarlos.

--- src/com/example/app/Grafica.java
+++ src/com/example/app/Grafica.java
@@ -9,8 +9,8 @@ import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
-
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
@@ -31,31 +31,42 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class Grafica extends Activity implements SensorEventListener {
-
+public class Grafica extends Activity implements SensorEventListener,
+		OnClickListener {
+	private static final String TAG = "Acelerometro aplicacion";
+	 private boolean[] mGraphs = { true, true, true };
 	// Conjunto de datos principal que incluye todas las series de la gráfica
 	XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
 	// Render que incluye el renderer personalizado de la gráfica
 	XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-
 	// Renderer creado más reciente, personalizando las series actuales
 	XYSeriesRenderer mRendererActual;
 	// Gráfica que mostrará los datos
 	GraphView mVistaGrafica;
-	
+	// Declaramos los colores del fondo, de las líneas y del texto
 	private int mBGColor;
 	private int mZeroLineColor;
 	private int mStringColor;
+	private int[] mAngleColors = new int[3];
 
-	int tipo;
-	ConcurrentLinkedQueue<float[]> datosSensor = new ConcurrentLinkedQueue<float[]>();
-	float [] xyz= new float[3];
 	
+	
+	// Int tipo es la frecuencia que recogemos de la actividad anterior
+	int tipo;
+	// Hacemos una cola FIFO con listas enlazadas
+	ConcurrentLinkedQueue<float[]> datosSensor = new ConcurrentLinkedQueue<float[]>();
+	ConcurrentLinkedQueue<float[]> mRawHistory = new ConcurrentLinkedQueue<float[]>();
+	// Aquí guardamos los valores de x,y,z en un array que luego irán el la cola
+	float[] xyz = new float[3];
+
 	// Declaración del layout
 	LinearLayout layout;
 	// Maneja los sensores
 	SensorManager sensorManager;
+	// Declaramos los botones
 	Button parar;
+	Button guardar;
+	Button enviar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,31 +77,70 @@ public class Grafica extends Activity implements SensorEventListener {
 		layout = (LinearLayout) findViewById(R.id.chart);
 		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		parar = (Button) findViewById(R.id.parar);
-
-		Resources resources = getResources();
+		guardar = (Button) findViewById(R.id.guardar);
+		enviar = (Button) findViewById(R.id.enviar);
 		
+		//Escuchamos los botones
+		parar.setOnClickListener(this);
+		guardar.setOnClickListener(this);
+		enviar.setOnClickListener(this);
+		Resources resources = getResources();
+
 		mStringColor = resources.getColor(R.color.string);
 		mBGColor = resources.getColor(R.color.background);
 		mZeroLineColor = resources.getColor(R.color.zero_line);
-		// Declaramos la escucha del botón parar
-		parar.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Parar_sensores();
-			}
-		});
+		mAngleColors[SensorManager.DATA_X] = resources
+                .getColor(R.color.accele_x);
+		mAngleColors[SensorManager.DATA_Y] = resources
+                .getColor(R.color.accele_y);
+		mAngleColors[SensorManager.DATA_Z] = resources
+                .getColor(R.color.accele_z);
 
 		// Recogemos el tipo de frecuencia que hemos pasado desde la actividad
 		// de acelerómetro
 		Bundle graficas = getIntent().getExtras();
 		tipo = graficas.getInt("tipo");
 
-		//declaramos la grafica
+		// declaramos la grafica
 		mVistaGrafica = new GraphView(this);
 		layout.addView(mVistaGrafica, 0);
+		onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		if (!mDrawRoop) {
+            // グラフの描画を再開
+            mDrawRoop = true;
+            mVistaGrafica.surfaceCreated(mVistaGrafica.getHolder());
+		}
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		mRawHistory = new ConcurrentLinkedQueue<float[]>();
 		Iniciar_sensores();
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		mRawHistory = new ConcurrentLinkedQueue<float[]>();
+		Parar_sensores();
+		super.onStop();
+	}
+
+	private void saveHistory() {
+		// Paramos los sensores
+		onStop();
+
+		// 保存用Threadを開始
+		SaveThread thread = new SaveThread();
+		thread.start();
 	}
 
 	protected void Iniciar_sensores() {
@@ -98,11 +148,13 @@ public class Grafica extends Activity implements SensorEventListener {
 				.registerListener(this, sensorManager
 						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), tipo);
 		mVistaGrafica.surfaceCreated(mVistaGrafica.getHolder());
+		
 	}
 
 	protected void Parar_sensores() {
 		sensorManager.unregisterListener(this,
 				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+		
 	}
 
 	@Override
@@ -114,28 +166,34 @@ public class Grafica extends Activity implements SensorEventListener {
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-		
-			switch (event.sensor.getType()) {
 
-			case Sensor.TYPE_ACCELEROMETER:
-				
-				
-				for (int i=0;i <3;i++) {
+		switch (event.sensor.getType()) {
+
+		case Sensor.TYPE_ACCELEROMETER:
+			mRawHistory.add(event.values.clone());
+
+			for (int i = 0; i < 3; i++) {
 				float valor = event.values[i];
 				xyz[i] = valor;
-				}
-				
-				synchronized (this) {
-					datosSensor.add(xyz.clone());
-Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
+			}
+
+			synchronized (this) {
+				 if (datosSensor.size() >= mMaxHistorySize) {
+                     datosSensor.poll();
+             }
+				datosSensor.add(xyz.clone());
+				Log.d("Valor xyz", "xxxx " + xyz[1] + "  puta " + datosSensor
+						+ "");
 			}
 		}
 	}
-	private class GraphView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
-		
+
+	private class GraphView extends SurfaceView implements
+			SurfaceHolder.Callback, Runnable {
+
 		private Thread hilo;
 		private SurfaceHolder holder;
-		
+
 		public GraphView(Context context) {
 			super(context);
 			// TODO Auto-generated constructor stub
@@ -150,97 +208,112 @@ Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
 		public void run() {
 			// TODO Auto-generated method stub
 
-			
+			 Log.i(TAG, "GraphView.run()");
 
-			Paint textPaint = new Paint();
-			textPaint.setColor(mStringColor);
-			textPaint.setAntiAlias(true);
-			textPaint.setTextSize(14);
+             int width = getWidth();
+             mMaxHistorySize = (int) ((width - 20) / mLineWidth);
 
-			Paint zeroLinePaint = new Paint();
-			zeroLinePaint.setColor(mZeroLineColor);
-			zeroLinePaint.setAntiAlias(true);
+             Paint textPaint = new Paint();
+             textPaint.setColor(mStringColor);
+             textPaint.setAntiAlias(true);
+             textPaint.setTextSize(14);
 
-		
+             Paint zeroLinePaint = new Paint();
+             zeroLinePaint.setColor(mZeroLineColor);
+             zeroLinePaint.setAntiAlias(true);
 
-		
-				Canvas canvas = holder.lockCanvas();
+             Paint[] linePaints = new Paint[4];
+             for (int i = 0; i < 3; i++) {
+                     linePaints[i] = new Paint();
+                     linePaints[i].setColor(mAngleColors[i]);
+                     linePaints[i].setAntiAlias(true);
+                     linePaints[i].setStrokeWidth(2);
+             }
 
-			
+             while (mDrawRoop) {
+                     Canvas canvas = holder.lockCanvas();
 
-				canvas.drawColor(mBGColor);
+                     if (canvas == null) {
+                             break;
+                     }
 
-				float zeroLineY = 230;
+                     canvas.drawColor(mBGColor);
 
-				synchronized (holder) {
-					float twoLineY = zeroLineY - (20 * 6);
-					float oneLineY = zeroLineY - (10 * 6);
-					float minasOneLineY = zeroLineY + (10 * 6);
-					float minasTwoLineY = zeroLineY + (20 * 6);
+                     float zeroLineY = mZeroLineY + mZeroLineYOffset;
 
-					canvas.drawText("2", 5, twoLineY + 5, zeroLinePaint);
-					canvas.drawLine(20, twoLineY, getWidth(), twoLineY,
-							zeroLinePaint);
+                     synchronized (holder) {
+                             float twoLineY = zeroLineY - (20 * mGraphScale);
+                             float oneLineY = zeroLineY - (10 * mGraphScale);
+                             float minasOneLineY = zeroLineY + (10 * mGraphScale);
+                             float minasTwoLineY = zeroLineY + (20 * mGraphScale);
 
-					canvas.drawText("1", 5, oneLineY + 5, zeroLinePaint);
-					canvas.drawLine(20, oneLineY, getWidth(), oneLineY,
-							zeroLinePaint);
+                             canvas.drawText("2", 5, twoLineY + 5, zeroLinePaint);
+                             canvas.drawLine(20, twoLineY, width, twoLineY,
+                                             zeroLinePaint);
 
-					canvas.drawText("0", 5, zeroLineY + 5, zeroLinePaint);
-					canvas.drawLine(20, zeroLineY, getWidth(), zeroLineY,
-							zeroLinePaint);
+                             canvas.drawText("1", 5, oneLineY + 5, zeroLinePaint);
+                             canvas.drawLine(20, oneLineY, width, oneLineY,
+                                             zeroLinePaint);
 
-					canvas.drawText("-1", 5, minasOneLineY + 5, zeroLinePaint);
-					canvas.drawLine(20, minasOneLineY, getWidth(), minasOneLineY,
-							zeroLinePaint);
+                             canvas.drawText("0", 5, zeroLineY + 5, zeroLinePaint);
+                             canvas.drawLine(20, zeroLineY, width, zeroLineY,
+                                             zeroLinePaint);
 
-					canvas.drawText("-2", 5, minasTwoLineY + 5, zeroLinePaint);
-					canvas.drawLine(20, minasTwoLineY, getWidth(), minasTwoLineY,
-							zeroLinePaint);
+                             canvas.drawText("-1", 5, minasOneLineY + 5, zeroLinePaint);
+                             canvas.drawLine(20, minasOneLineY, width, minasOneLineY,
+                                             zeroLinePaint);
 
-					if (datosSensor.size() > 1) {
-						Iterator<float[]> iterator = datosSensor.iterator();
-						float[] before = new float[4];
-						int x = getWidth() - datosSensor.size() * 2;
-						int beforeX = x;
-						x += 2;
+                             canvas.drawText("-2", 5, minasTwoLineY + 5, zeroLinePaint);
+                             canvas.drawLine(20, minasTwoLineY, width, minasTwoLineY,
+                                             zeroLinePaint);
 
-						if (iterator.hasNext()) {
-							float[] history = iterator.next();
-							for (int angle = 0; angle < 4; angle++) {
-								before[angle] = zeroLineY
-										- (history[angle] * 6);
-							}
-							while (iterator.hasNext()) {
-								history = iterator.next();
-								for (int angle = 0; angle < 4; angle++) {
-									float startY = zeroLineY
-											- (history[angle] * 6);
-									
-									before[angle] = startY;
-								}
-								beforeX = x;
-								x += 2;
-							}
-						}
-					}
-				}
+                             if (datosSensor.size() > 1) {
+                                     Iterator<float[]> iterator = datosSensor.iterator();
+                                     float[] before = new float[3];
+                                     int x = width - datosSensor.size() * mLineWidth;
+                                     int beforeX = x;
+                                     x += mLineWidth;
 
-				holder.unlockCanvasAndPost(canvas);
+                                     if (iterator.hasNext()) {
+                                             float[] history = iterator.next();
+                                             for (int angle = 0; angle < 3; angle++) {
+                                                     before[angle] = zeroLineY
+                                                                     - (history[angle] * mGraphScale);
+                                             }
+                                             while (iterator.hasNext()) {
+                                                     history = iterator.next();
+                                                     for (int angle = 0; angle < 3; angle++) {
+                                                             float startY = zeroLineY
+                                                                             - (history[angle] * mGraphScale);
+                                                             float stopY = before[angle];
+                                                             if (mGraphs[angle]) {
+                                                                     canvas.drawLine(x, startY, beforeX,
+                                                                                     stopY, linePaints[angle]);
+                                                             }
+                                                             before[angle] = startY;
+                                                     }
+                                                     beforeX = x;
+                                                     x += mLineWidth;
+                                             }
+                                     }
+                             }
+                     }
 
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					
-				}
-			}
+                     holder.unlockCanvasAndPost(canvas);
+
+                     try {
+                             Thread.sleep(mDrawDelay);
+                     } catch (InterruptedException e) {
+                             Log.e(TAG, e.getMessage());
+                     }
+             }
+             }
 
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
-			
-		}
 
+		}
 
 		@Override
 		public void surfaceChanged(SurfaceHolder holder, int format, int width,
@@ -255,18 +328,19 @@ Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
 			// TODO Auto-generated method stub
 			boolean roop = true;
 			while (roop) {
-					try {
-						hilo.join();
-						roop= false;
-						
-					} catch (InterruptedException e) { }
+				try {
+					hilo.join();
+					roop = false;
+
+				} catch (InterruptedException e) {
+				}
 			}
 			hilo = null;
-			
+
 		}
-		
-		
+
 	}
+
 	private class SaveThread extends Thread {
 		@Override
 		public void run() {
@@ -290,20 +364,20 @@ Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
 
 			try {
 				// SDã‚«ãƒ¼ãƒ‰ã«ãƒ‡ã‚£ãƒ¬ã‚¯ãƒˆãƒªãŒãªã‘ã‚Œã°ä½œæˆ
-				String appName = "app";
-				String dirPath = Environment.getExternalStorageDirectory()
-						.toString()
-						+ "/" + appName;
+				String appName = getResources().getString(R.string.app_name);
+				String dirPath = Environment.getExternalStorageDirectory().toString() + "/" + appName;
 				File dir = new File(dirPath);
 				if (!dir.exists()) {
 					dir.mkdirs();
 				}
 
 				// CSVãƒ•ã‚¡ã‚¤ãƒ«åã‚’ä½œæˆ
-				String fileName = DateFormat.format("yyyyMMddkkmmss", System.currentTimeMillis()).toString().concat(".csv");
-				
+				String fileName = DateFormat
+						.format("yyyyMMddkkmmss", System.currentTimeMillis())
+						.toString().concat(".csv");
+
 				// CSVãƒ•ã‚¡ã‚¤ãƒ«ã¸ä¿å­˜
-				File file = new File(dirPath, fileName.toString());
+				File file = new File(dirPath, fileName);
 				if (file.createNewFile()) {
 					FileOutputStream fileOutputStream = new FileOutputStream(
 							file);
@@ -312,21 +386,42 @@ Log.d("Valor xyz", "xxxx "+ xyz[1]+"  puta "+ datosSensor+"");
 					fileOutputStream.close();
 				}
 
-				// ãƒ•ã‚¡ã‚¤ãƒ«ã¸ã®å‡ºåŠ›å®Œäº†ã‚’é€šçŸ¥
-				bundle.putString("msg", "GUARDADO");
-				bundle.putBoolean("success", true);
 			} catch (Exception e) {
-			
-
-				// ãƒ•ã‚¡ã‚¤ãƒ«ã¸ã®å‡ºåŠ›å¤±æ•—ã‚’é€šçŸ¥
-				bundle.putString("msg", e.getMessage());
+				Log.e(TAG, e.getMessage());
+				//bundle.putString("msg", e.getMessage());
 				bundle.putBoolean("success", false);
 			}
 
 			// ãƒãƒ³ãƒ‰ãƒ©ã‚’ä½¿ã£ã¦ãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ãƒ³ã‚°
 			msg.setData(bundle);
-		
+
 		}
 	}
-}
 
+	@Override
+	public void onClick(View boton) {
+		// TODO Auto-generated method stub
+		switch (boton.getId()) {
+		case (R.id.parar):
+			onStop();
+			break;
+		case (R.id.guardar):
+			Log.d("algo","boton guardar");
+			saveHistory();
+			break;
+		case (R.id.enviar):
+			break;
+		}
+	}
+
+	private boolean mDrawRoop = true;
+	        private int mDrawDelay = 100;
+	        private int mLineWidth = 2;
+	        private int mGraphScale = 6;
+	        private int mZeroLineY = 230;
+	        private int mZeroLineYOffset = 0;
+
+			private int mMaxHistorySize;
+				
+
+}
