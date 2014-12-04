cf28d00b1b7135178a772094bd89e274915054bc
FELIX <felixhuete@iMac-de-FELIX.local>
2014-12-04 20:26:42 +0100

    4-12-2014 funciona gráfica
    creacion de la clase ticker , y funcionamiento de la gráfica

--- src/com/example/app/Grafica.java
+++ src/com/example/app/Grafica.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
-import org.achartengine.chart.PointStyle;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
@@ -15,7 +14,10 @@ import org.achartengine.renderer.XYSeriesRenderer;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -26,21 +28,29 @@ import android.os.Message;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Grafica extends Activity implements SensorEventListener,
-		OnClickListener {
+public class Grafica extends Activity implements OnClickListener {
 	private static final String TAG = "Acelerometro aplicacion";
 	
 	// declarar grafica
 
 	private GraphicalView chartView;
-	XYMultipleSeriesDataset sensorData = new XYMultipleSeriesDataset();
-	XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+	XYMultipleSeriesDataset sensorData;
+	XYMultipleSeriesRenderer mRenderer;
 	XYSeries series[];
 	public static final int SAMPLERATE = 10;
+	
+	
+	/**
+	 * For moving the viewport of the grpah
+	 */
+	private int lastMinX = 0;
 
 	
 	long timestamp;
@@ -52,7 +62,12 @@ public class Grafica extends Activity implements SensorEventListener,
 	
 	// Aquí guardamos los valores de x,y,z en un array que luego irán el la cola
 	float[] xyz = new float[3];
+	
+	public static final String SENSORINDEX = "com.example.app.SensorIndex";
 
+
+	private Thread ticker;
+	private Sensor sensor;
 	// Declaración del layout
 	LinearLayout layout;
 	// Maneja los sensores
@@ -66,6 +81,9 @@ public class Grafica extends Activity implements SensorEventListener,
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		
 		setContentView(R.layout.grafica);
 		// Declaramos objetos
 		layout = (LinearLayout) findViewById(R.id.chart);
@@ -78,32 +96,32 @@ public class Grafica extends Activity implements SensorEventListener,
 		parar.setOnClickListener(this);
 		guardar.setOnClickListener(this);
 		enviar.setOnClickListener(this);
-
+		sensorData = new XYMultipleSeriesDataset();
+		mRenderer = new XYMultipleSeriesRenderer();
 		//grafica
-		/*mRenderer.setGridColor(Color.DKGRAY);
-		mRenderer.setShowGrid(true);
-		mRenderer.setApplyBackgroundColor(true);
-	    mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-		//mRenderer.setXAxisMin(0.0);
-		mRenderer.setXTitle("xyz");
-		mRenderer.setXAxisMax(10); // 10 seconds wide
-		mRenderer.setXLabels(10); // 1 second per DIV
-		mRenderer.setChartTitle(" ");
-		mRenderer.setZoomButtonsVisible(true);
-		mRenderer.setYLabelsAlign(Paint.Align.RIGHT);
-		chartView = ChartFactory.getLineChartView(this, sensorData, mRenderer);*/
+		//chartView = ChartFactory.getLineChartView(this, sensorData, mRenderer);
 		
 		mRenderer.setGridColor(Color.DKGRAY);
 		mRenderer.setShowGrid(true);
-		mRenderer.setApplyBackgroundColor(true);
-		mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-		mRenderer.setAxisTitleTextSize(16);
-		mRenderer.setChartTitleTextSize(20);
-		mRenderer.setLabelsTextSize(15);
-		mRenderer.setLegendTextSize(15);
-		//renderer.setMargins(new int[] { 20, 30, 15, 0 });
-		mRenderer.setZoomButtonsVisible(true);
-		mRenderer.setPointSize(3);
+		mRenderer.setXAxisMin(0.0);
+		mRenderer.setXTitle(getString(R.string.samplerate, 1000 / SAMPLERATE));
+		mRenderer.setXAxisMax(10000 / (1000 / SAMPLERATE)); // 10 seconds wide
+		mRenderer.setXLabels(10); // 1 second per DIV
+		mRenderer.setChartTitle(" ");
+		mRenderer.setYLabelsAlign(Paint.Align.RIGHT);
+		chartView = ChartFactory.getLineChartView(this, sensorData, mRenderer);
+		float textSize = new TextView(this).getTextSize();
+		float upscale = textSize / mRenderer.getLegendTextSize();
+		mRenderer.setLabelsTextSize(textSize);
+		mRenderer.setLegendTextSize(textSize);
+		mRenderer.setChartTitleTextSize(textSize);
+		mRenderer.setAxisTitleTextSize(textSize);
+		mRenderer.setFitLegend(true);
+		int[] margins = mRenderer.getMargins();
+		margins[0] *= upscale;
+		margins[1] *= upscale;
+		margins[2] = (int) (2 * mRenderer.getLegendTextSize());
+		mRenderer.setMargins(margins);
 		
 		//chartView.repaint();
 		//layout.addView(chartView);
@@ -118,19 +136,135 @@ public class Grafica extends Activity implements SensorEventListener,
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		 super.onResume();
-		 
+		 switch (getResources().getConfiguration().orientation) {
+			case Configuration.ORIENTATION_PORTRAIT: {
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+				break;
+			}
+			case Configuration.ORIENTATION_LANDSCAPE: {
+				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+				break;
+			}
+		}
+
+		if (xTick == 0) {
+			ticker = new Ticker(this);
+			ticker.start();
+			sensorManager.registerListener((SensorEventListener) ticker, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+					tipo);
+		}
+	}
+	
+	protected void onTick(SensorEvent currentEvent) {
+
+		if (xTick == 0) {
+			// Dirty, but we only learn a few things after getting the first event.
+			configure(currentEvent);
+			layout.addView(chartView);
+		}
+
+		if (xTick > mRenderer.getXAxisMax()) {
+			mRenderer.setXAxisMax(xTick);
+			mRenderer.setXAxisMin(++lastMinX);
+		}
+
+		fitYAxis(currentEvent);
+
+		for (int i = 0; i < series.length; i++) {
+			if (series[i] != null) {
+				series[i].add(xTick, currentEvent.values[i]);
+			}
+		}
+		xTick++;
+		
+		for (int i = 0; i < 3; i++) {
+			float valor = currentEvent.values[i];
+			xyz[i] = valor;
+			Log.d("hola","xxx "+i + " "+xyz[i] +"");
+		}
+		synchronized (this) {
+			datosSensor.add(xyz.clone());
+			timestamp = System.currentTimeMillis();
+			//configure(datosSensor);
+			
+			}
+
+		
+		chartView.repaint();
+	}
+	
+	private void fitYAxis(SensorEvent event) {
+		double min = mRenderer.getYAxisMin(), max = mRenderer.getYAxisMax();
+		for (int i = 0; i < series.length; i++) {
+			if (event.values[i] < min) {
+				min = event.values[i];
+			}
+			if (event.values[i] > max) {
+				max = event.values[i];
+			}
+		}
+		float sum = 0;
+		for (int i = 0; i < event.values.length; i++) {
+			sum += event.values[i];
+		}
+		double half = 0;
+		if (xTick == 0 && sum == event.values[0] * event.values.length) {
+			// If the plot flatlines on the first event, we can't grade the Y axis.
+			// This is especially bad if the sensor does not change without a
+			// stimulus. the graph will then flatline on the x-axis where it is
+			// impossible to be seen.
+			half = event.values[0] * 0.5 + 1;
+		}
+		mRenderer.setYAxisMax(max + half);
+		mRenderer.setYAxisMin(min - half);
 	}
 
+	private void configure(SensorEvent event) {
+		String[] channelNames = new String[event.values.length];
+		series = new XYSeries[event.values.length];
+		for (int i = 0; i < channelNames.length; i++) {
+			channelNames[i] = getString(R.string.channel_default) + i;
+		}
+
+		switch (event.sensor.getType()) {
+			case Sensor.TYPE_ACCELEROMETER: {
+				channelNames[0] = getString(R.string.channel_x_axis);
+				channelNames[1] = getString(R.string.channel_y_axis);
+				channelNames[2] = getString(R.string.channel_z_axis);
+				mRenderer.setYTitle(getString(R.string.unit_acceleration));
+				break;
+			}
+			
+		}
+
+		int[] colors = {
+				Color.RED,
+				Color.YELLOW,
+				Color.BLUE,
+				Color.GREEN,
+				Color.MAGENTA,
+				Color.CYAN };
+		for (int i = 0; i < series.length; i++) {
+			series[i] = new XYSeries(channelNames[i]);
+			sensorData.addSeries(series[i]);
+			XYSeriesRenderer r = new XYSeriesRenderer();
+			r.setColor(colors[i % colors.length]);
+			mRenderer.addSeriesRenderer(r);
+			
+
+		}
+	}
+	
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
-		Iniciar_sensores();
 		super.onStart();
 	}
 
 	@Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
+		
 		Parar_sensores();
 		super.onStop();
 	}
@@ -144,95 +278,22 @@ public class Grafica extends Activity implements SensorEventListener,
 		thread.start();
 	}
 
-	protected void Iniciar_sensores() {
-		sensorManager
-				.registerListener(this, sensorManager
-						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), tipo);
-
-	}
 
 	protected void Parar_sensores() {
-		sensorManager.unregisterListener(this,
-				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
-		
+		try {
+			sensorManager.unregisterListener((SensorEventListener) ticker);
+			ticker.interrupt();
+			ticker.join();
+			ticker = null;
+			Toast.makeText(this, "Parado", Toast.LENGTH_SHORT).show();
+		}
+		catch (Exception e) {
+		}
 	}
 
-	@Override
-	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		// TODO Auto-generated method stub
-
-	}
 	
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
-		
-		
-		switch (event.sensor.getType()) {
-
-		case Sensor.TYPE_ACCELEROMETER:{
-			for (int i = 0; i < 3; i++) {
-				float valor = event.values[i];
-				xyz[i] = valor;
-				Log.d("hola","xxx "+i + " "+xyz[i] +"");
-				
-			}
-			
-			synchronized (this) {
-				datosSensor.add(xyz.clone());
-				timestamp = System.currentTimeMillis();
-				//configure(datosSensor);
-				XYSeries xSeries = new XYSeries("X");
-				XYSeries ySeries = new XYSeries("Y");
-				XYSeries zSeries = new XYSeries("Z");
-				
-				xSeries.add(timestamp, event.values[0]);
-				ySeries.add(timestamp, event.values[1]);
-				zSeries.add(timestamp, event.values[2]);
-				
-			
-				sensorData.addSeries(xSeries);
-				sensorData.addSeries(ySeries);
-				sensorData.addSeries(zSeries);
-
-				XYSeriesRenderer xRenderer = new XYSeriesRenderer();
-				xRenderer.setColor(Color.RED);
-				xRenderer.setPointStyle(PointStyle.CIRCLE);
-				xRenderer.setFillPoints(true);
-				xRenderer.setLineWidth(3);
-				xRenderer.setDisplayChartValues(false);
-
-				XYSeriesRenderer yRenderer = new XYSeriesRenderer();
-				yRenderer.setColor(Color.GREEN);
-				yRenderer.setPointStyle(PointStyle.CIRCLE);
-				yRenderer.setFillPoints(true);
-				yRenderer.setLineWidth(3);
-				yRenderer.setDisplayChartValues(false);
-
-				XYSeriesRenderer zRenderer = new XYSeriesRenderer();
-				zRenderer.setColor(Color.BLUE);
-				zRenderer.setPointStyle(PointStyle.CIRCLE);
-				zRenderer.setFillPoints(true);
-				zRenderer.setLineWidth(3);
-				zRenderer.setDisplayChartValues(false);
-				
-				
-				
-				mRenderer.addSeriesRenderer(xRenderer);
-				mRenderer.addSeriesRenderer(yRenderer);
-				mRenderer.addSeriesRenderer(zRenderer);
-				
-				chartView = ChartFactory.getLineChartView(this, sensorData, mRenderer);
-				layout.addView(chartView);
-				chartView.repaint();
-				
-				}
-			
-			break;
-		}
-		}
-	}
+	
 	
 
 
@@ -291,8 +352,8 @@ public class Grafica extends Activity implements SensorEventListener,
 			msg.setData(bundle);
 
 		}
+	
 	}
-
 	@Override
 	public void onClick(View boton) {
 		// TODO Auto-generated method stub
@@ -310,6 +371,4 @@ public class Grafica extends Activity implements SensorEventListener,
 		}
 	}
 
-
-
-}
+	}
