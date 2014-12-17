d8ff04b1548f784f8d970f6023bf8b4555efdbe4
FELIX <felixhuete@iMac-de-FELIX.local>
2014-12-17 20:13:09 +0100

    17-12-2014
    BORRAR CLASES SOBRANTES
    NUEVO DISEÑO
    CREACION DE LAS PREFERENCIAS

--- gen/com/example/app/R.java
+++ gen/com/example/app/R.java
@@ -9,19 +9,24 @@ package com.example.app;
 
 public final class R {
     public static final class array {
-        public static final int frecuencia_recogida=0x7f060000;
+        public static final int frecuencia_recogida=0x7f070000;
+        public static final int frecuencias=0x7f070001;
+        public static final int opcionestiempograbacion=0x7f070005;
+        public static final int temporizador=0x7f070002;
+        public static final int temporizadoropciones=0x7f070003;
+        public static final int tiempograbacion=0x7f070004;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int accele_r=0x7f040003;
-        public static final int accele_x=0x7f040000;
-        public static final int accele_y=0x7f040001;
-        public static final int accele_z=0x7f040002;
-        public static final int background=0x7f040006;
-        public static final int filter_rate_value=0x7f040007;
-        public static final int string=0x7f040005;
-        public static final int zero_line=0x7f040004;
+        public static final int accele_r=0x7f050003;
+        public static final int accele_x=0x7f050000;
+        public static final int accele_y=0x7f050001;
+        public static final int accele_z=0x7f050002;
+        public static final int background=0x7f050006;
+        public static final int filter_rate_value=0x7f050007;
+        public static final int string=0x7f050005;
+        public static final int zero_line=0x7f050004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -31,8 +36,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -42,53 +47,54 @@ public final class R {
         public static final int psensor=0x7f020004;
     }
     public static final class id {
-        public static final int TextView01=0x7f0a000c;
-        public static final int acelerometer=0x7f0a001e;
-        public static final int acelerometro=0x7f0a001f;
-        public static final int acercaDe=0x7f0a002b;
-        public static final int action_settings=0x7f0a002a;
-        public static final int buttons=0x7f0a0013;
-        public static final int chart=0x7f0a0011;
-        public static final int config=0x7f0a002c;
-        public static final int detecta=0x7f0a0025;
-        public static final int ejemplos=0x7f0a0029;
-        public static final int ejex=0x7f0a0017;
-        public static final int ejey=0x7f0a0018;
-        public static final int ejez=0x7f0a0019;
-        public static final int enviar=0x7f0a002e;
-        public static final int etTiempo=0x7f0a000a;
-        public static final int frecuencias=0x7f0a0002;
-        public static final int giroscopio=0x7f0a0020;
-        public static final int giroscopo=0x7f0a0021;
-        public static final int gps=0x7f0a0006;
-        public static final int gpss=0x7f0a0004;
-        public static final int grafica=0x7f0a000b;
-        public static final int guardar=0x7f0a002d;
-        public static final int infoacelerometro=0x7f0a0001;
-        public static final int infogiroscopio=0x7f0a0010;
-        public static final int infoluz=0x7f0a001b;
-        public static final int infomagnetico=0x7f0a001c;
-        public static final int infoproximidad=0x7f0a001d;
-        public static final int inicio=0x7f0a0014;
-        public static final int linear=0x7f0a0012;
-        public static final int luminosidad=0x7f0a0028;
-        public static final int luminosity=0x7f0a0027;
-        public static final int magnetic=0x7f0a0022;
-        public static final int magnetico=0x7f0a0023;
-        public static final int medicion=0x7f0a000d;
-        public static final int modulo=0x7f0a001a;
-        public static final int parar=0x7f0a0015;
-        public static final int proximidad=0x7f0a0026;
-        public static final int proximity=0x7f0a0024;
-        public static final int reiniciar=0x7f0a0016;
-        public static final int scrollView1=0x7f0a0000;
-        public static final int sensoresdisponibles=0x7f0a000e;
-        public static final int spinnerfrecuencia=0x7f0a0003;
-        public static final int teoria=0x7f0a000f;
-        public static final int tiempo=0x7f0a0007;
-        public static final int tiempos=0x7f0a0009;
-        public static final int toggleGps=0x7f0a0005;
-        public static final int toggleTiempo=0x7f0a0008;
+        public static final int TextView01=0x7f0b000c;
+        public static final int acelerometer=0x7f0b001a;
+        public static final int acelerometro=0x7f0b001b;
+        public static final int acercaDe=0x7f0b002b;
+        public static final int button1=0x7f0b001c;
+        public static final int button2=0x7f0b001f;
+        public static final int button3=0x7f0b0022;
+        public static final int button4=0x7f0b0025;
+        public static final int button5=0x7f0b0028;
+        public static final int buttons=0x7f0b0012;
+        public static final int chart=0x7f0b0010;
+        public static final int config=0x7f0b002c;
+        public static final int configurate=0x7f0b002f;
+        public static final int detecta=0x7f0b0024;
+        public static final int ejemplos=0x7f0b002a;
+        public static final int ejex=0x7f0b0016;
+        public static final int ejey=0x7f0b0017;
+        public static final int ejez=0x7f0b0018;
+        public static final int enviar=0x7f0b002e;
+        public static final int etTiempo=0x7f0b000a;
+        public static final int frecuencias=0x7f0b0002;
+        public static final int giroscopio=0x7f0b001d;
+        public static final int giroscopo=0x7f0b001e;
+        public static final int gps=0x7f0b0006;
+        public static final int gpss=0x7f0b0004;
+        public static final int grafica=0x7f0b000b;
+        public static final int guardar=0x7f0b002d;
+        public static final int infoacelerometro=0x7f0b0001;
+        public static final int inicio=0x7f0b0013;
+        public static final int linear=0x7f0b0011;
+        public static final int luminosidad=0x7f0b0029;
+        public static final int luminosity=0x7f0b0027;
+        public static final int magnetic=0x7f0b0020;
+        public static final int magnetico=0x7f0b0021;
+        public static final int medicion=0x7f0b000d;
+        public static final int modulo=0x7f0b0019;
+        public static final int parar=0x7f0b0014;
+        public static final int proximidad=0x7f0b0026;
+        public static final int proximity=0x7f0b0023;
+        public static final int reiniciar=0x7f0b0015;
+        public static final int scrollView1=0x7f0b0000;
+        public static final int sensoresdisponibles=0x7f0b000e;
+        public static final int spinnerfrecuencia=0x7f0b0003;
+        public static final int teoria=0x7f0b000f;
+        public static final int tiempo=0x7f0b0007;
+        public static final int tiempos=0x7f0b0009;
+        public static final int toggleGps=0x7f0b0005;
+        public static final int toggleTiempo=0x7f0b0008;
     }
     public static final class layout {
         public static final int acelerometro=0x7f030000;
@@ -99,61 +105,57 @@ public final class R {
         public static final int defluz=0x7f030005;
         public static final int defmagnetico=0x7f030006;
         public static final int defproximidad=0x7f030007;
-        public static final int giroscopio=0x7f030008;
-        public static final int grafica=0x7f030009;
-        public static final int list=0x7f03000a;
-        public static final int luz=0x7f03000b;
-        public static final int magnetico=0x7f03000c;
-        public static final int proximidad=0x7f03000d;
-        public static final int simulacion=0x7f03000e;
-        public static final int teoria=0x7f03000f;
+        public static final int grafica=0x7f030008;
+        public static final int list=0x7f030009;
+        public static final int simulacion=0x7f03000a;
+        public static final int teoria=0x7f03000b;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
-        public static final int menu=0x7f090001;
-        public static final int menugrafica=0x7f090002;
+        public static final int menu=0x7f0a0000;
+        public static final int menugrafica=0x7f0a0001;
+        public static final int menusimulacion=0x7f0a0002;
     }
     public static final class string {
-        public static final int acelerometro=0x7f07000e;
-        public static final int acercaDe=0x7f070004;
-        public static final int action_settings=0x7f070002;
-        public static final int app_name=0x7f070000;
-        public static final int channel_default=0x7f07001a;
-        public static final int channel_x_axis=0x7f070017;
-        public static final int channel_y_axis=0x7f070018;
-        public static final int channel_z_axis=0x7f070019;
-        public static final int config=0x7f070003;
-        public static final int cuenta=0x7f07000c;
-        public static final int ejemplos=0x7f070006;
-        public static final int empezar=0x7f07000a;
-        public static final int enviar=0x7f070013;
-        public static final int explicacion=0x7f070008;
-        public static final int frecuencias=0x7f07001e;
-        public static final int giroscopio=0x7f070011;
-        public static final int gpss=0x7f07001c;
-        public static final int guardar=0x7f070014;
-        public static final int hello_world=0x7f070001;
-        public static final int infoacelerometro=0x7f070020;
-        public static final int infogiroscopio=0x7f070021;
-        public static final int infoluz=0x7f070024;
-        public static final int infomagnetico=0x7f070022;
-        public static final int infoproximidad=0x7f070023;
-        public static final int luminosidad=0x7f07000f;
-        public static final int magnetico=0x7f070012;
-        public static final int medicion=0x7f070005;
-        public static final int modulo=0x7f070027;
-        public static final int mostrar=0x7f07000d;
-        public static final int parar=0x7f07000b;
-        public static final int proximidad=0x7f070010;
-        public static final int reanudar=0x7f070026;
-        public static final int reiniciar=0x7f070025;
-        public static final int samplerate=0x7f07001f;
-        public static final int save_complate=0x7f070015;
-        public static final int save_imcomplate=0x7f070016;
-        public static final int sensores=0x7f070009;
-        public static final int teoria=0x7f070007;
-        public static final int tiempo=0x7f07001d;
-        public static final int unit_acceleration=0x7f07001b;
+        public static final int acelerometro=0x7f08000e;
+        public static final int acercaDe=0x7f080004;
+        public static final int action_settings=0x7f080002;
+        public static final int app_name=0x7f080000;
+        public static final int channel_default=0x7f08001a;
+        public static final int channel_x_axis=0x7f080017;
+        public static final int channel_y_axis=0x7f080018;
+        public static final int channel_z_axis=0x7f080019;
+        public static final int config=0x7f080003;
+        public static final int cuenta=0x7f08000c;
+        public static final int ejemplos=0x7f080006;
+        public static final int empezar=0x7f08000a;
+        public static final int enviar=0x7f080013;
+        public static final int explicacion=0x7f080008;
+        public static final int frecuencias=0x7f08001e;
+        public static final int giroscopio=0x7f080011;
+        public static final int gpss=0x7f08001c;
+        public static final int guardar=0x7f080014;
+        public static final int hello_world=0x7f080001;
+        public static final int infoacelerometro=0x7f080020;
+        public static final int infogiroscopio=0x7f080021;
+        public static final int infoluz=0x7f080024;
+        public static final int infomagnetico=0x7f080022;
+        public static final int infoproximidad=0x7f080023;
+        public static final int luminosidad=0x7f08000f;
+        public static final int magnetico=0x7f080012;
+        public static final int medicion=0x7f080005;
+        public static final int modulo=0x7f080027;
+        public static final int mostrar=0x7f08000d;
+        public static final int parar=0x7f08000b;
+        public static final int proximidad=0x7f080010;
+        public static final int reanudar=0x7f080026;
+        public static final int reiniciar=0x7f080025;
+        public static final int samplerate=0x7f08001f;
+        public static final int save_complate=0x7f080015;
+        public static final int save_imcomplate=0x7f080016;
+        public static final int sensores=0x7f080009;
+        public static final int teoria=0x7f080007;
+        public static final int tiempo=0x7f08001d;
+        public static final int unit_acceleration=0x7f08001b;
     }
     public static final class style {
         /** 
@@ -177,13 +179,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-        public static final int MooSpinnerItem=0x7f080003;
-        public static final int MooTextAppearanceSpinnerItem=0x7f080004;
-        public static final int MooTheme=0x7f080002;
+        public static final int AppTheme=0x7f090001;
+        public static final int MooSpinnerItem=0x7f090003;
+        public static final int MooTextAppearanceSpinnerItem=0x7f090004;
+        public static final int MooTheme=0x7f090002;
+    }
+    public static final class xml {
+        public static final int confsimulacion=0x7f040000;
     }
 }
--- src/com/example/app/Simulacion.java
+++ src/com/example/app/Simulacion.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -9,6 +10,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -30,13 +34,19 @@ public class Simulacion extends Activity implements SensorEventListener,
 	Button proximity;
 	Button luminosity;
 	Sensor giroscope;
+	Sensor aceleromete;
+	Sensor magnetometro;
+	Sensor luces;
+	Sensor proximo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.simulacion);
-
+		
+		// Declarar textView y botones
+		
 		detecta = (TextView) findViewById(R.id.detecta);
 		acelerometro = (TextView) findViewById(R.id.acelerometro);
 		giroscopo = (TextView) findViewById(R.id.giroscopo);
@@ -49,8 +59,16 @@ public class Simulacion extends Activity implements SensorEventListener,
 		proximity = (Button) findViewById(R.id.proximity);
 		luminosity = (Button) findViewById(R.id.luminosity);
 		
+		
+		// declarar los sensores como variables
+		
 		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		giroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		magnetometro = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		luces = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		proximo = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+		aceleromete = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
 		// Escucha de los botones
 		acelerometer.setOnClickListener(this);
 		giroscopio.setOnClickListener(this);
@@ -61,33 +79,33 @@ public class Simulacion extends Activity implements SensorEventListener,
 
 	@Override
 	public void onClick(View boton) {
-		// TODO Auto-generated method stub
+		// al pulsar los botones nos lleva a la explicacion del sensor.
 		switch (boton.getId()) {
 		case R.id.acelerometer:
-			Intent acelerometro = new Intent(this, Acelerometro.class);
+			Intent acelerometro = new Intent(this, DefinicionAcelerometro.class);
 			startActivity(acelerometro);
 			break;
 		case R.id.giroscopio:
 			
-			Intent gravedad = new Intent(Simulacion.this,Giroscopio.class);
+			Intent gravedad = new Intent(Simulacion.this,DefinicionGiroscopio.class);
 			startActivity(gravedad);
 			
 			break;
 		case R.id.magnetic:
 			
-			 Intent magnetico = new Intent(Simulacion.this,Magnetico.class);
+			 Intent magnetico = new Intent(Simulacion.this,DefinicionMagnetico.class);
 			 startActivity(magnetico);
 			
 			break;
 		case R.id.luminosity:
 			
-			 Intent luz = new Intent(Simulacion.this, Luz.class);
+			 Intent luz = new Intent(Simulacion.this, DefinicionLuz.class);
 			 startActivity(luz);
 			
 			break;
 		case R.id.proximity:
 			
-			 Intent proximidad = new Intent(Simulacion.this, Proximidad.class);
+			 Intent proximidad = new Intent(Simulacion.this, DefinicionProximidad.class);
 			 startActivity(proximidad);
 			
 			break;
@@ -95,29 +113,48 @@ public class Simulacion extends Activity implements SensorEventListener,
 	}
 
 	// Iniciamos cada uno de los sensores que vamos a usar
+	
 	protected void Iniciar_sensores() {
+		//iniciar acelerometro
+		if (aceleromete== null) {
+			acelerometer.setText("\nNO ESTA DISPONIBLE EL SENSOR\n");
+		} else{
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
 				SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		//iniciar giroscopio
 		if (giroscope== null) {
-			giroscopo.setText("NO ESTA DISPONIBLE EL SENSOR");
+			giroscopo.setText("\nNO ESTA DISPONIBLE EL SENSOR\n");
 		} else{
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
 				SensorManager.SENSOR_DELAY_NORMAL);
 	    }	
+		//iniciar magnetometro
+		if (magnetometro== null) {
+			magnetic.setText("\nNO ESTA DISPONIBLE EL SENSOR\n");
+		} else{
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
 				SensorManager.SENSOR_DELAY_NORMAL);
-
+		}
+		//iniciar proximidad
+		if (proximo== null) {
+			proximity.setText("\nNO ESTA DISPONIBLE EL SENSOR\n");
+		} else{
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),
 				SensorManager.SENSOR_DELAY_NORMAL);
-
+		}
+		//iniciar luces
+		if (luces== null) {
+			luminosidad.setText("\nNO ESTA DISPONIBLE EL SENSOR\n");
+		} else{
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
 				SensorManager.SENSOR_DELAY_NORMAL);
-
+		}
 	}
 
 	// Paramos todos los sensores
@@ -143,7 +180,7 @@ public class Simulacion extends Activity implements SensorEventListener,
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-		String txt = "\n\nSensor: ";
+		String txt = "\nSensor: ";
 		synchronized (this) {
 			Log.d("sensor", event.sensor.getName());
 			
@@ -163,42 +200,30 @@ public class Simulacion extends Activity implements SensorEventListener,
 				txt += "\n x: " + event.values[0] + " rad/s";
 				txt += "\n y: " + event.values[1] + " rad/s";
 				txt += "\n z: " + event.values[2] + " rad/s";
-				giroscopo.setText(txt);
-				if (event == null) {
-
-					giroscopo.setText("No esta disponible");
-				}
-
-				
+				giroscopo.setText(txt);				
 				break;
 
 			case Sensor.TYPE_MAGNETIC_FIELD:
 				txt += "Campo magnético\n";
 				txt += "\n" + event.values[0] + " µT";
-
 				magnetico.setText(txt);
-
 				break;
+			
 			case Sensor.TYPE_PROXIMITY:
 				txt += "Proximidad\n";
 				txt += "\n" + event.values[0] + " cm";
-
 				proximidad.setText(txt);
-
 				// Si detecta 0 lo represento
 				if (event.values[0] == 0) {
-
 					detecta.setBackgroundColor(Color.parseColor("#cf091c"));
 					detecta.setText("Proximidad Detectada");
 				}
-
 				break;
+				
 			case Sensor.TYPE_LIGHT:
 				txt += "Luminosidad\n";
 				txt += "\n" + event.values[0] + " Lux";
-
 				luminosidad.setText(txt);
-
 				break;
 			}
 
@@ -207,51 +232,65 @@ public class Simulacion extends Activity implements SensorEventListener,
 	}
 
 	protected void onStop() {
-
+		// parar sensores
 		Parar_sensores();
-
 		super.onStop();
 	}
 
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
-
 		Parar_sensores();
-
 		super.onDestroy();
 	}
 
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
-
 		Parar_sensores();
-
 		super.onPause();
 	}
 
-	@Override
-	protected void onRestart() {
-		// TODO Auto-generated method stub
-
-		Iniciar_sensores();
-
-		super.onRestart();
-
-	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
 		Iniciar_sensores();
-
+		SharedPreferences pref= getSharedPreferences("com.example.app.Preferencias", MODE_PRIVATE);
+		int temporizador,tiempo;
+		String Tipo;
+		
+		String prefTiempo = pref.getString("tiempo", "0");
+	
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
 	}
+	                
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Llamamos al menusimulacion
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menusimulacion, menu);
+		return true;
+		/** true -> el menú ya está visible */
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		//Llamamos a la clase Preferencias
+		switch (item.getItemId()) {
+		case R.id.configurate:
+			Intent i = new Intent(this, Preferencias.class);
+			startActivity(i);
+			break;
+		}
+		return true;
+		/** true -> consumimos el item, no se propaga */
+	}
+	
+	
 }
